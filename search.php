44b72a4ace1002f0d32f609768bc892f6ae2714e
kreftingmusic <134586708+kreftingmusic@users.noreply.github.com>
2023-05-25 10:31:57 +0200

    Update search.php

--- search.php
+++ search.php
@@ -1,1 +1,18 @@
+<?php
+// Verbindung zur Datenbank herstellen
+$conn = mysqli_connect("localhost", "username", "password", "database");
 
+// Suchanfrage verarbeiten
+if (isset($_GET['query'])) {
+    $query = mysqli_real_escape_string($conn, $_GET['query']);
+    $sql = "SELECT * FROM content WHERE content LIKE '%$query%'";
+    $result = mysqli_query($conn, $sql);
+}
+
+// Suchergebnisse anzeigen
+if (isset($result)) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<a href='".$row['url']."'>".$row['title']."</a><br>";
+    }
+}
+?>
